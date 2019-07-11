076eec9e7074f5efba0e6b7460415caca991c71e
junqiangchen <1207173174@qq.com>
2019-07-11 14:50:41 +0800

    Update utils.py

--- KiTS19Challege/dataprocess/utils.py
+++ KiTS19Challege/dataprocess/utils.py
@@ -33,7 +33,7 @@ def file_name_path(file_dir, dir=True, file=False):
 
 def save_file2csv(file_dir, file_name):
     """
-    save file path to csv
+    save file path to csv,this is for segmentation
     :param file_dir:preprocess data path
     :param file_name:output csv name
     :return:
